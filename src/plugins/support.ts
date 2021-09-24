cc2c2e17c453a32dc9032ae044c080fda05db4d6
Floyd Jones <floyd-jones1@hotmail.com>
2021-09-23 22:27:04 -0400

    implemented mongodb

--- src/plugins/support.ts
+++ src/plugins/support.ts
@@ -1,22 +1,36 @@
-import fp from 'fastify-plugin'
+import fp from "fastify-plugin";
+import fastifyJwt from "fastify-jwt";
+import mongo from "fastify-mongodb";
 
 export interface SupportPluginOptions {
-  thing: string
+  thing: string;
   // Specify Support plugin options here
 }
 
 // The use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 export default fp<SupportPluginOptions>(async (fastify, opts) => {
-  console.log(opts)
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
-  })
-})
+  // console.log(opts)
+  // fastify.decorate('someSupport', function (foo: number) {
+  //   return 'hugs'
+  // })
+  fastify.register(fastifyJwt, { secret: "supersecret" });
+
+  fastify.register(mongo, {
+    forceClose: true,
+    url: "mongodb://root:example@localhost:27017",
+    database: "database",
+    auth: {
+      password: "example",
+      username: "root",
+    },
+  });
+});
 
 // When using .decorate you have to specify added properties for Typescript
-declare module 'fastify' {
+declare module "fastify" {
   export interface FastifyInstance {
     someSupport(): string;
+    // jwtVerify(): Function
   }
 }
--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,11 +1,21 @@
-import { FastifyPluginAsync } from "fastify"
+import { FastifyPluginAsync } from "fastify";
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  console.log(opts)
-  fastify.get('/', async function (request, reply) {
-    fastify.
-    return 'this is an login'
-  })
-}
+  const usersCollection = fastify.mongo.db?.collection("users");
+  fastify.post("/login", async function (request, reply) {
+    usersCollection?.findOne({ id: 1 }, (err, user) => {
+      console.log("here");
+      if (err || !user) {
+        reply.status(500).send(err);
+      }
+      reply.status(200).send(user);
+    });
+    return reply;
+  });
+
+  fastify.post("/register", async function (request, reply) {
+    return;
+  });
+};
 
 export default example;
